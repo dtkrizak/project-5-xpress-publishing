3988890c9e0ff7588b6a54267b580138884c3ce1
Daniel Krizak <dtkrizak@me.com>
2021-03-03 13:24:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# capstone-project
+ Cumulative API project
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const errorHandler = require('errorhandler');
+const sqlite3 = require('sqlite3');
+const cors = require('cors');
+const apiRouter = require('./api/api');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(errorHandler());
+app.use(cors());
+app.use('/api', apiRouter);
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+    console.log(`server is listening on port: ${PORT}`);
+})
+
+module.exports = app;
